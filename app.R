5fd18fba65f1b350605d3145bf73e23e69507b77
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2020-12-04 16:37:59 -0800

    Update app.R

--- app.R
+++ app.R
@@ -1026,7 +1026,7 @@ server <- function(input, output, session){
         confirmButtonCol = "#AEDEF4",
         cancelButtonText = "No Thanks!",
         timer = 0,
-        imageUrl = "https://www.festivalclaca.cat/imgfv/b/537-5374275_bee-insect-drawing-flying-wings-isolated-honeybee-free.png",
+        imageUrl = "https://img.icons8.com/plasticine/100/000000/insect.png",
         imageWidth = 100,
         imageHeight = 100,
         animation = TRUE, 
